df6650ce3ad9df7ff8404ef8cd61c2c275a07676
wcwdfu <skb9731@gmail.com>
2024-11-19 12:24:01 +0900

    o

--- src/main/java/org/example/auctiongameclient/AuctionManager.java
+++ src/main/java/org/example/auctiongameclient/AuctionManager.java
@@ -14,16 +14,16 @@ public class AuctionManager {
 
     public void participateInAuction() {
         out.println("참가");
-        messageArea.appendText("경매에 참가 요청을 보냈습니다.\n");
+        messageArea.appendText("당신은 경매에 응찰 했습니다.\n");
     }
 
     public void declineAuction() {
         out.println("불참여");
-        messageArea.appendText("경매에 불참 요청을 보냈습니다.\n");
+        messageArea.appendText("당신은 경매에 불응찰 했습니다.\n");
     }
 
     public void placeBid(int bidAmount) {
         out.println("호가 " + bidAmount);
-        messageArea.appendText("입찰 요청을 보냈습니다: " + bidAmount + "원\n");
+//        messageArea.appendText("입찰 요청을 보냈습니다: " + bidAmount + "원\n");
     }
 }
